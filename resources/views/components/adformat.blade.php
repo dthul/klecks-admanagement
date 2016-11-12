06b7b5fb29d4f62cc0ea42b59e5edcf0e81455ce
Daniel Thul <daniel.thul@gmail.com>
2016-11-12 22:37:13 +0100

    Fix price input field styling.

--- resources/views/components/adformat.blade.php
+++ resources/views/components/adformat.blade.php
@@ -11,16 +11,12 @@
     <form method="POST" action="{{ !$create ? route('adformats.update', $adformat->id) : route('adformats.create', $issue->id) }}" autocomplete="off">
         {!! csrf_field() !!}
         <label for="{{ $id }}_name">Name</label>
-        <input type="text" value="{{ !$create ? $adformat->name : '' }}" placeholder="1/2 Seite bunt" name="name" id="{{ $id }}_name">
-        <div class="row collapse">
-            <label for="{{ $id }}_price">Preis</label>
-            <div class="small-9 columns">
-                <input type="number" value="{{ !$create ? $adformat->price : '' }}" placeholder="1000" name="price" id="{{ $id }}_price">
-            </div>
-            <div class="small-3 columns">
-                <span class="postfix">Cent</span>
-            </div>
-        </div>
+		<input type="text" value="{{ !$create ? $adformat->name : '' }}" placeholder="1/2 Seite bunt" name="name" id="{{ $id }}_name">
+		<label for="{{ $id }}_price">Preis</label>
+		<div class="input-group">
+			<input type="number" value="{{ !$create ? $adformat->price : '' }}" placeholder="1000" name="price" id="{{ $id }}_price" class="input-group-field">
+			<span class="input-group-label">Cent</span>
+		</div>
         <button type="submit" class="button">{{ $create ? 'Anlegen' : 'Änderung speichern' }}</button>
     </form>
 </div>
