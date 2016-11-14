87d6324a7d32afa1f6343c524fffc5092b8d4915
Daniel Thul <daniel.thul@gmail.com>
2016-11-14 01:23:20 +0100

    Style invoice.

--- resources/views/layouts/invoice.blade.php
+++ resources/views/layouts/invoice.blade.php
@@ -3,82 +3,107 @@
 <head>
     <meta charset="utf-8">
     <title>Rechnung</title>
-    <link href="https://fonts.googleapis.com/css?family=Open+Sans:300,400" rel="stylesheet" type="text/css">
     <link rel="stylesheet" href="{{ asset('css/invoice.css') }}">
+    <link href="https://fonts.googleapis.com/css?family=Open+Sans:400,700" rel="stylesheet" type="text/css">
     <link rel="license" href="http://www.opensource.org/licenses/mit-license/">
     {{--<script src="{{ asset('js/invoice.js') }}"></script>--}}
 </head>
 <body>
-<header>
-    <h1>Rechnung</h1>
-    <address>
-        <p>Schülerzeitung Der Klecks</p>
-        <p>Abt-Richard-Straße XX</p>
-        <p>54550 Daun</p>
-    </address>
-    <span><img alt="" src="logo.png"></span>
-</header>
-<article>
-    <h1>Recipient</h1>
-    <address>
-        <p>{!! nl2br(e($customer->address)) !!}</p>
-    </address>
-    <table class="meta">
-        <tr>
-            <th>Rechnung #</th>
-            <td>{{ $issue->id }}-{{ $customer->id }}</td>
-        </tr>
-        <tr>
-            <th>Datum</th>
-            <td>January 1, 2012</td>
-        </tr>
-        <tr>
-            <th>Fällig am</th>
-            <td>January 1, 2012</td>
-        </tr>
-    </table>
-    <table class="inventory">
-    <thead>
-        <tr>
-            <th>Werbeformat</th>
-            <th>Seite</th>
-            <th>Preis</th>
-        </tr>
-    </thead>
-    <tbody>
-    <?php $total = 0; ?>
-    @foreach ($advertisements as $advertisement)
-        <tr>
-            <td>{{ $advertisement->adformat->name }}</td>
-            <td>{{ $advertisement->page }}</td>
-            <td>{{ $advertisement->adformat->price / 100 }}€</td>
-        </tr>
-        <?php $total += $advertisement->adformat->price; ?>
-    @endforeach
-    </tbody>
-    </table>
-    <table class="balance">
-        <tr>
-            <th>Gesamt</th>
-            <td>{{ $total / 100 }}€</td>
-        </tr>
-        {{--
-        <tr>
-            <th><span>Amount Paid</span></th>
-            <td><span>$</span><span>0.00</span></td>
-        </tr>
-        <tr>
-            <th><span>Balance Due</span></th>
-            <td><span>$</span><span>600.00</span></td>
-        </tr>
-        --}}
-    </table>
-</article>
-<aside>
-    <h1><span>Additional Notes</span></h1>
-    <div>
-        <p>A finance charge of 1.5% will be made on unpaid balances after 30 days.</p>
+<div class="page">
+    <div class="header">
+        <h1>Der Klecks</h1>
+        <span><img alt="" src="logo.png"></span>
     </div>
-</aside>
+    <div class="window">
+        <address class="sender">
+            <p>TMG - Freiherr-vom-Stein-Str. 14 - 54550 Daun</p>
+        </address>
+        <address class="recipient">
+            <p>{!! nl2br(e($customer->address)) !!}</p>
+        </address>
+    </div>
+    <div class="info">
+        <p>
+            Schülerzeitung Der Klecks<br>
+            Thomas-Morus-Gymnasium<br>
+            Freiherr-vom-Stein-Str. 14<br>
+            54550 Daun
+        <p>
+        <p>
+            Tel.: +49 (6592) 98350-35<br>
+            klecks@???
+        </p>
+        <p>
+            Datum: {{ date('Y-m-d') }}
+        </p>
+    </div>
+    <div class="text">
+        <p class="subject">
+            Rechnung Nr. {{ $issue->id }}-{{ $customer->id }}
+        </p>
+        <p class="salutation">
+            Sehr geehrter Werbekunde,
+        <p>
+        <p>
+            für Ihre Werbeanzeige in der aktuellen Ausgabe der Schülerzeitung "Der Klecks" erlauben wir uns, folgende Positionen in Rechnung zu stellen.
+        </p>
+        <table class="listing">
+        <thead>
+            <tr>
+                <th>Werbeformat</th>
+                <th>Seite</th>
+                <th class="text-right">Preis</th>
+            </tr>
+        </thead>
+        <tbody>
+        @foreach ($advertisements as $advertisement)
+            <tr>
+                <td>{{ $advertisement->adformat->name }}</td>
+                <td>{{ $advertisement->page }}</td>
+                <td class="text-right">{{ $advertisement->adformat->price / 100 }}€</td>
+            </tr>
+        @endforeach
+        </tbody>
+        <tfoot>
+            <tr>
+                <td></td>
+                <td colspan="2" class="text-right">Summe {{ $advertisements->pluck('adformat')->pluck('price')->sum() / 100 }} €</td>
+            </tr>
+        </tfoot>
+        </table>
+        <p>
+            Bitte überweisen Sie den Rechnungsbetrag innerhalb von 14 Tagen.
+        </p>
+        <p>
+            Wir danken recht herzlich für Ihren Auftrag.
+        </p>
+        <p>
+            Mit freundlichen Grüßen,<br>
+            Die Redaktion des Klecks
+        </p>
+    </div>
+    <div class="pagenumber">
+        <p>Seite 1 von 1</p>
+    </div>
+    <div class="footer">
+        <p>
+            Schülerzeitung Der Klecks<br>
+            Thomas-Morus-Gymnasium<br>
+            Freiherr-vom-Stein-Str. 14<br>
+            54550 Daun
+        </p>
+        <p>
+            Bank: Kreissparkasse Vulkaneifel<br>
+            IBAN: DE11 9900 0000 8888 7777 00
+        </p>
+        <p>
+            Email: klecks@???<br>
+            Telefon: +49 (6592) 98350-35
+        </p>
+    </div>
+    <hr class="page-marker-1">
+    <hr class="page-marker-2">
+    <hr class="page-marker-3">
+</page>
 </body>
 </html>
