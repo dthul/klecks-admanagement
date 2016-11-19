ea5b756143e5c4561879b541f4861e9849954218
Daniel Thul <daniel.thul@gmail.com>
2016-11-19 12:40:35 +0100

    Minor modifications to the deploy script.

--- deploy.sh
+++ deploy.sh
@@ -1,9 +1,8 @@
 #!/bin/bash
 DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
 DEST=klecks@euve251339.serverprofi24.de:/home/klecks/www
-RSYNC="rsync -rlptEz --exclude-from $DIR/rsync.exclude"
+RSYNC="rsync --recursive --links --safe-links --perms --executability --times --compress --exclude-from $DIR/rsync.exclude"
 
-$RSYNC --del -v $DIR/app $DIR/bootstrap $DIR/database $DIR/public $DIR/resources $DIR/routes $DEST
-$RSYNC -v $DIR/storage .env.example artisan $DEST
-$RSYNC --ignore-existing -v $DIR/config $DEST
+$RSYNC --del --verbose $DIR/app $DIR/bootstrap $DIR/config $DIR/database $DIR/public $DIR/resources $DIR/routes $DEST
+$RSYNC --verbose $DIR/storage .env.example artisan $DEST
 $RSYNC --del $DIR/vendor $DEST
